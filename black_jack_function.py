15fe8ea6e9cb529ba1940816d2010aa12868e5f3
Youki <nucacola8@gmail.com>
2023-09-23 22:37:11 +0900

    Created a function that enables to bet multiple hands

--- black_jack_function.py
+++ black_jack_function.py
@@ -1,17 +1,12 @@
 import random
-trump = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-trump_value = {"A": [1, 11], "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, 
-"10": 10, "J": 10, "Q": 10, "K": 10}
-chip_variation = {"1": 5, "2": 10, "3": 25, "4": 50, "5": 100, "6": 500}
 
-def set_game(choosing_spot = False):
-    if choosing_spot == True:
-        print("""
-                    Dealer      
-1:Spot1   2:Spot2   3:Spot3   4:Spot4   5:Spot5
-""")
-        spot_number = choose_spot()
-        bet = betting()
+class Hand:
+    def __init__(self, spot_number, bet):
+        self.number = spot_number
+        self.bet = bet
+        self.hand = [draw_card() for i in range(0, 2) ]
+        # for i in range(0, 3):
+            # self.hand.append(drew_card())
 
 def choose_spot():
     finish_choosing = False
@@ -23,28 +18,39 @@ def choose_spot():
         else:
             print("Sorry, Spot{} is not at this table".format(spot_number))
 
-def betting():
+def betting(player_chip):
     finish_betting = False
+    limit = player_chip
     bet = 0
     while finish_betting == False:
         print("Bet:$", bet)
         chip = input("""
-1:$5, 2:$10, 3:$25, 4:$50, 5:$100, 6:$500, 7:All-in, 0:Finish betting
+1:$5, 2:$10, 3:$25, 4:$50, 5:$100, 6:$500, 7:All-in, 
+0:Finish betting
 Enter the number__""")
         if int(chip) in range(1, 7):
-            bet += chip_variation[chip]
+            if limit >= chip_variation[chip]:
+                bet += chip_variation[chip]
+                limit -= chip_variation[chip]
+            else:
+                print("You seem not to have enough chip")
         elif int(chip) == 7:
-            bet = player_chip
+            bet += player_chip
             finish_betting = True
             return bet
         elif int(chip) == 0:
-            if bet <= 0:
+            if bet == 0:
                 print("You can't bet $0")
             else:
                 finish_betting = True
                 return bet
         else:
             print("Please enter correct number")
+
+trump = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+trump_value = {"A": [1, 11], "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, 
+"10": 10, "J": 10, "Q": 10, "K": 10}
+chip_variation = {"1": 5, "2": 10, "3": 25, "4": 50, "5": 100, "6": 500}
 
 def draw_card():
     return random.choice(trump)
--- black_jack_main.py
+++ black_jack_main.py
@@ -1,9 +1,40 @@
 import black_jack_function
 player_chip = 1000
+player_hands = []
 
-black_jack_function.set_game(True)
+def set_multiple_hands():
+    finish_setting = False
+    spot_already_taken = [False, False, False, False, False]
+    while finish_setting == False:
+        number_of_spots = 1
+        for i in spot_already_taken:
+            if i == False:
+                print("Spot", number_of_spots, " is available to bet")
+                number_of_spots += 1
+            else:
+                print("Spot", number_of_spots, " is already taken")
+                number_of_spots += 1
+        spot_number = black_jack_function.choose_spot()
+        bet = black_jack_function.betting(player_chip)
+        player_hands.append(black_jack_function.Hand(spot_number, bet))
+        spot_already_taken[spot_number - 1] = True
+        for hand in player_hands:
+            print("Spot{} : Bet{}".format(int(hand.number), int(hand.bet)))
+        correct_letter = False
+        end_setting = input("Enter Y for finishing setting or any other letter for betting another spot")
+        if end_setting.upper() == "Y":
+            finish_setting = True
 
 
+
+
+
+set_multiple_hands()
+for hand in player_hands:
+    print(hand.hand)
+    
+
+# black_jack_function.set_game(True)
 # player_hand = []
 # dealer_hand = []
 # for i in range(0, 2):
